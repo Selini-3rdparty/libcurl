2909d59573b06a2788b6763b4b151685202cac5b
Tom Dudzik <tom@selinicapital.com>
2023-10-11 19:51:43 +0000

    Fix for building libcurl with c-ares submodule style

---
+++ tests/libtest/CMakeFiles/chkdecimalpoint.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tom/sln/libcurl/lib/curl_ctype.c" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/curl_ctype.c.o" "gcc" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/curl_ctype.c.o.d"
+  "/home/tom/sln/libcurl/lib/dynbuf.c" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/dynbuf.c.o" "gcc" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/dynbuf.c.o.d"
+  "/home/tom/sln/libcurl/lib/mprintf.c" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/mprintf.c.o" "gcc" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/mprintf.c.o.d"
+  "/home/tom/sln/libcurl/lib/strdup.c" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/strdup.c.o" "gcc" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/__/__/lib/strdup.c.o.d"
+  "/home/tom/sln/libcurl/tests/libtest/chkdecimalpoint.c" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/chkdecimalpoint.c.o" "gcc" "tests/libtest/CMakeFiles/chkdecimalpoint.dir/chkdecimalpoint.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/server/CMakeFiles/sockfilt.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tom/sln/libcurl/lib/base64.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/base64.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/base64.c.o.d"
+  "/home/tom/sln/libcurl/lib/curl_ctype.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/curl_ctype.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/curl_ctype.c.o.d"
+  "/home/tom/sln/libcurl/lib/curl_multibyte.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/curl_multibyte.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/curl_multibyte.c.o.d"
+  "/home/tom/sln/libcurl/lib/dynbuf.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/dynbuf.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/dynbuf.c.o.d"
+  "/home/tom/sln/libcurl/lib/inet_pton.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/inet_pton.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/inet_pton.c.o.d"
+  "/home/tom/sln/libcurl/lib/memdebug.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/memdebug.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/memdebug.c.o.d"
+  "/home/tom/sln/libcurl/lib/mprintf.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/mprintf.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/mprintf.c.o.d"
+  "/home/tom/sln/libcurl/lib/nonblock.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/nonblock.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/nonblock.c.o.d"
+  "/home/tom/sln/libcurl/lib/strdup.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/strdup.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/strdup.c.o.d"
+  "/home/tom/sln/libcurl/lib/strtoofft.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/strtoofft.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/strtoofft.c.o.d"
+  "/home/tom/sln/libcurl/lib/timediff.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/timediff.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/timediff.c.o.d"
+  "/home/tom/sln/libcurl/lib/warnless.c" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/warnless.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/__/__/lib/warnless.c.o.d"
+  "/home/tom/sln/libcurl/tests/server/getpart.c" "tests/server/CMakeFiles/sockfilt.dir/getpart.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/getpart.c.o.d"
+  "/home/tom/sln/libcurl/tests/server/sockfilt.c" "tests/server/CMakeFiles/sockfilt.dir/sockfilt.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/sockfilt.c.o.d"
+  "/home/tom/sln/libcurl/tests/server/util.c" "tests/server/CMakeFiles/sockfilt.dir/util.c.o" "gcc" "tests/server/CMakeFiles/sockfilt.dir/util.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
